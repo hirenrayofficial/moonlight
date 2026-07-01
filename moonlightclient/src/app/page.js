2ada421d5695f10bad9d9f9a8e9c06054f685d76
Hiren Ray <hirenray882@gmail.com>
2026-07-02 00:14:30 +0530

    add phase cp

--- moonlightclient/src/app/page.js
+++ moonlightclient/src/app/page.js
@@ -20,7 +20,7 @@ export default function page() {
 
       {/* <ProductPage /> */}
       {/* <HeroDesign /> */}
-      {/* <HowItWorks/> */}
+      <HowItWorks/>
       {/* <SectionServices /> */}
       {/* <ProductPA /> */}
       <StackScroll />
