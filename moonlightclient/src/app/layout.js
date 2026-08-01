16d3ad5fad2fece8ba421642ccbc8078db5a3f40
Hiren Ray <hirenray882@gmail.com>
2026-08-01 08:39:09 +0530

    add tag manager

--- moonlightclient/src/app/layout.js
+++ moonlightclient/src/app/layout.js
@@ -37,14 +37,33 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable}  h-full antialiased`}
     >
-      {/* <header>
-   
-        <Header />
-      </header> */}
-      <body className="min-h-full bg-[#faf9f5] flex flex-col "><Queryprovider>  {children}</Queryprovider></body>
-      {/* <footer>
-        <Footer />
-      </footer> */}
+      <header>
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-5ZKB7HST');
+            `,
+          }}
+        />
+      </header>
+
+      <body className="min-h-full bg-[#faf9f5] flex flex-col ">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5ZKB7HST"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript><Queryprovider>  {children}</Queryprovider>
+      </body>
+
     </html>
   );
 }
