63e319a2dff8bb1b0b48a17f90d61320b6f65daf
Hiren Ray <hirenray882@gmail.com>
2026-09-16 18:42:24 +0530

    solved error

--- moonlightclient/src/app/blog/[slug]/page.js
+++ moonlightclient/src/app/blog/[slug]/page.js
@@ -13,7 +13,7 @@ async function getProduct(slug) {
 
     try {
         // 1st arg: URL, 2nd arg: Request Body (empty object since you use query params), 3rd arg: Config
-        const res = await axios.post(`${SITE_URL}/api/home/blog/get?slug=${slug}`, {});
+        const res = await axios.post(`/api/home/blog/get?slug=${slug}`, {});
 
         // Axios stores parsed response data in res.data
         // Since Supabase .single() returns a single object, item is an object (not an array)
